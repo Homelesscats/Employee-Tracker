a25c11c2f6feb713be330a1e5b19672febf49469
homelesscat <gilbertperezdt@gmail.com>
2023-06-28 17:05:44 -0700

    added inquirer

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2'); 
-
+const inquirer = require('inquirer');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,10 +15,30 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username,
-    user: 'root',
+    user: 'CEDRIC',
     // MySQL password
     password: '',
-    database: 'books_db'
+    database: 'employee_tracker_db'
   },
   console.log(`Connected to the books_db database.`)
 );
+
+
+
+/* 
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  //   ])
+  //  .then((answers) => {
+    // Use user feedback for... whatever!!
+  //  })
+  //  .catch((error) => {
+    //  if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    // } else {
+      // Something else went wrong
+    // }
+ // });
+  */
